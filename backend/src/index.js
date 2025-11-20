61fb833bb837e5e94d60521cd87434ef674e4823
RKdevelopers-app <gpushkar05@gmail.com>
2025-11-20 17:29:57 +0530

    push

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,28 +1,28 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import path from "path";
 
-import {connectDB} from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
-import { app, server } from './lib/socket.js';
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
-  
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-
+     
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-})
+    credentials: true,
+  })
 );
 
 app.use("/api/auth", authRoutes);
@@ -36,8 +36,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-
-server.listen(5001, ()=> {
-    console.log("server started on PORT " + PORT);
-    connectDB();
-})
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
